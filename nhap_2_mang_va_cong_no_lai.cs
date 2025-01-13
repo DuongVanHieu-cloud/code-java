8fd74fa02d30da57fa8ac02f57a5dfde9b9515ce
DuongVanHieu-cloud <hieuduong6428@gmail.com>
2025-01-13 21:11:31 +0700

    Update nhap_2_mang_va_cong_no_lai.cs

--- nhap_2_mang_va_cong_no_lai.cs
+++ nhap_2_mang_va_cong_no_lai.cs
@@ -63,3 +63,53 @@ class mang {
 
     }
 }
+//////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Xml.Schema;
+using System.Linq;
+class mang {
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        
+
+        int[] a = new int[n];// tao mang voi kich thuoc n
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = int.Parse(Console.ReadLine());
+        }
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            b[i] = int.Parse(Console.ReadLine());
+        }
+        int[] c = new int[a.Length + b.Length];
+        for(int i = 0;i< a.Length; i++)
+        {
+            c[i] = a[i] ;
+        }
+        for (int i = 0;i< b.Length; i++)
+        {
+            c[i + a.Length] = b[i];
+        }
+        Console.WriteLine(string.Join(", ",c));
+        for (int i = 0;i <c.Length;i++)
+        {
+            for (int j = i+1;j< c.Length; j++)
+            {
+                if (c[j] < c[i])
+                {
+                    int temp = c[i];
+                    c[i] = c[j];
+                    c[j] = temp;
+                }
+            }
+        }
+        Console.WriteLine(string.Join(", ", c));
+    }
+}
+
