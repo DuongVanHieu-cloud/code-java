4cc10aff8a01ef83e3c2c5a3c9dc56c60bab2085
DuongVanHieu-cloud <hieuduong6428@gmail.com>
2025-01-13 20:21:33 +0700

    a
    b

---
+++ New folder/dem_co_bao_nhieu_a_trong_mang.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Xml.Schema;
+using System.Linq;
+class mang {
+    public static void Main(string[] args)
+    {
+        int a = 2;
+        
+        int[] ints = { 2, 2, 3 };
+        int sum = Array.FindAll(ints,x => x == a).Length;
+        Console.WriteLine(sum);
+    }
+    
+    
+
+}
