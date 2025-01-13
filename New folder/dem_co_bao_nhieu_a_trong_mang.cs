6b6a3285dfc5cea964ad9b7976130d6a5cdbf85f
DuongVanHieu-cloud <hieuduong6428@gmail.com>
2025-01-13 20:54:14 +0700

    Update dem_co_bao_nhieu_a_trong_mang.cs

--- New folder/dem_co_bao_nhieu_a_trong_mang.cs
+++ New folder/dem_co_bao_nhieu_a_trong_mang.cs
@@ -14,7 +14,33 @@ class mang {
         int sum = Array.FindAll(ints,x => x == a).Length;
         Console.WriteLine(sum);
     }
-    
-    
-
+}
+////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Xml.Schema;
+using System.Linq;
+class mang {
+    public static void Main(string[] args)
+    {
+        int a = 2;
+        
+        int[] ints = { 2, 2, 3 };
+        var counts = new Dictionary<int, int>();
+        foreach (int value in ints)
+        {
+            if (counts.ContainsKey(value))
+            {
+                counts[value]++;
+            }
+            else
+            {
+                counts[value] = 0;
+            }
+        }
+        Console.WriteLine("So lan xuat hien cua {0} trong mang la: {1}", a, counts.ContainsKey(a) ? counts[a] : 0);
+    }
 }
