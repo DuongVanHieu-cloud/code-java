8162a9fcdb3a11aeec735ad25f0b1b4186425bb4
DuongVanHieu-cloud <hieuduong6428@gmail.com>
2025-01-13 20:55:07 +0700

    Add files via upload

---
+++ nhap_2_mang_va_cong_no_lai.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Xml.Schema;
+using System.Linq;
+class mang {
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        
+
+        int[] a = new int[n];// tao mang voi kich thuoc n
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = int.Parse(Console.ReadLine());
+        }
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            b[i] = int.Parse(Console.ReadLine());
+        }
+        int[] mergedArray = a.Concat(b).ToArray();
+        Console.Write(string.Join(", ", mergedArray));
+
+    }
+}
